28d256d6cc6ee8646229c92336e7c36954339d27
Sameeh <sameehkhan1@gmail.com>
2018-11-15 11:28:15 -0800

    Add new views page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
 
 
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+
+  def show
+
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
