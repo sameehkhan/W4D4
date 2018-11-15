4d63b02a3a9e91a4f49e5355111929f551c8f36f
Sameeh <sameehkhan1@gmail.com>
2018-11-15 15:28:01 -0800

    Add background

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -7,14 +7,14 @@ class BandsController < ApplicationController
   def create
     @band = Band.new(band_params)
     if @band.save
-      redirect_to band_url
+      redirect_to band_url(@band)
     else
       render json: @band.errors.full_messages, status: 422
     end
   end
 
   def new
-
+    render :new
   end
 
   def edit
@@ -22,12 +22,8 @@ class BandsController < ApplicationController
   end
 
   def show
-    @band = Band.find_by(id: params[:id])
-      if @band
-        render :show
-      else
-        redirect_to bands_url
-      end
+    @band = Band.find(params[:id])
+    render :show
   end
 
   def update
@@ -35,7 +31,7 @@ class BandsController < ApplicationController
   end
 
   def destroy
-
+    
   end
 
   def band_params
