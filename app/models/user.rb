01f6b5e575d8cd059f0e02353393501d99178e6c
Sameeh <sameehkhan1@gmail.com>
2018-11-15 10:59:32 -0800

    Finish user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,30 +4,39 @@ class User < ApplicationRecord
   validates :password_digest, presence: { message: 'Must provide password'}
   before_validation :ensure_session_token
 
-  # def self.find_by_credentials(email, password)
-  #   user = User.find_by(email: email)
-  #     if user
-  #
-  # end
+  attr_reader :password
 
-  def self.generate_session_token
 
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+      return user
+    else
+      return nil
+    end
   end
 
-  def reset_session_token!
+  # def self.generate_session_token
 
+  # end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
   end
 
   def ensure_session_token
-    
+    self.session_token ||= SecureRandom::urlsafe_base64
   end
 
   def password=(password)
-
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
-    Bcrypt
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
 
