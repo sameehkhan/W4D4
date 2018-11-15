3ccc30e1a3887103455078a4c1b9129b6b9d7264
Sameeh <sameehkhan1@gmail.com>
2018-11-15 10:40:31 -0800

    Add bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,34 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates :password, length: {minimun: 6, allow_nil: true}
+  validates :password_digest, presence: { message: 'Must provide password'}
+  before_validation :ensure_session_token
+
+  # def self.find_by_credentials(email, password)
+  #   user = User.find_by(email: email)
+  #     if user
+  #
+  # end
+
+  def self.generate_session_token
+
+  end
+
+  def reset_session_token!
+
+  end
+
+  def ensure_session_token
+    
+  end
+
+  def password=(password)
+
+  end
+
+  def is_password?(password)
+    Bcrypt
+  end
+
+
 end
